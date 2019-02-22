77899231f444a1d64777e5da20a5559905e7dedf
Eva Frisell <evmo1600@student.miun.se>
2019-02-22 18:24:44 +0100

    Set up css template

--- WahlbergUr/WahlbergUr/Controllers/HomeController.cs
+++ WahlbergUr/WahlbergUr/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace WahlbergUr.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Products()
         {
             return View();
         }
